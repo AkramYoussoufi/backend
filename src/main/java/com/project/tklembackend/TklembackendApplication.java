6c2ab66f552db8bdb755908302b65ad708008edb
Akram Youssoufi <akramyoussoufi1@gmail.com>
2024-02-03 21:46:49 +0100

    setting up environements

---
+++ src/main/java/com/project/tklembackend/TklembackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.tklembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TklembackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TklembackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/tklembackend/TklembackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.tklembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TklembackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
